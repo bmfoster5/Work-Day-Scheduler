9cf34b95cd1b33a4bf53468d0581328a5c0fa1d7
bmfoster5 <foster.brandon96@gmail.com>
2022-11-14 22:30:25 -0500

    second commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Work Day Scheduler
+
+## Description
+
+Hello! I have created a Work Day Scheduler! Take a look and let me know what you think.
+
+- My motivation for this project is to showcase an organized planner for someone who may need a visual represenation of what they need to complete throughout the course of a week.
+- I learned that through creating functions, if statements, accesing the local storage, and using web APIs, I can make a webpage that 
+- With the help of methods, I can object properties to organize and cause specific actions in my application. 
+
+
+## Usage
+
+![alt text](assets/media/Password-Generator.gif)
+
+## Live link
+
+
+## How to Contribute
+
+Let me know what I could improve on!
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,42 @@ var userInput = $('.description');
 var workTodo = [];
 // var date = date.gethours();
 var timeBox = document.querySelector('.container-lg');
+var currentTime = new Date();
+var currentHour = currentTime.getHours();
+var timeBlock = $(".time-block")
+var hour = dayjs().hour();
 
-
+currentDay();
+    setInterval(currentDay, 1000);
 // TODO: Add code to display the current date in the header of the page.
 function currentDay() {
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
-    
 }
 
-$(function () {
-    saveButton.addEvenListener("submit", function() {});
-  var workText = userInput.value.trim();
-  if (workText === "") {
-    return;
-  }
+$(document).ready($(function () {
 
-  storeText();
+    saveButton.on('click', function(event) {
+        event.preventdefault();
+        var workText = userInput.value.trim();
+        if (workText === "") {
+            return;
+    });
 
-
+    function colorChange() {
+        $(timeBlock).each(function(){
+          var timeSlot = parseInt($(this).attr('id').split('-')[1]);
+          if (timeSlot = hour) {
+            timeBlock.classList.add('present')
+          } else if (timeSlot > hour) {
+            timeBlock.classList.add('future')
+          } else {
+            timeBlock.classList.add('past')
+          }
+        })
+      }
+      
+      colorChange()
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -33,32 +50,56 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+  
+
+        storeText();
+
+        // TODO: Add code to apply the past, present, or future class to each time
+        // block by comparing the id to the current hour. HINTS: How can the id
+        // attribute of each time-block be used to conditionally add or remove the
+        // past, present, and future classes? How can Day.js be used to get the
+        // current hour in 24-hour time?
+        function init() {
+            var storedToDos = JSON.parse(localStorage.getItem(storedToDos));
+        
+
+           
+
+        }
+    };
+
+function colorChange() {
+$('.time-block').each(function(){
+    // assign var to each hour //use split
+    var timeSlot = 
+    if (storedTo !== null) {
+        workTodo = storedToDos;
+        // add if/else statement to have color code work
+
+    }
+    if (___ < currentHour) {
+        timeBlock.classList.add("past");
+        console.log("working")
+    }
+    if (___ == currentHour) {
+
+    } else {
+
+    }
     
+}); 
+
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    function init() {
-        var storedToDos = JSON.parse(localStorage.getItem(storedToDos));
-
-        if (storedToDos !== null) {
-            workTodo = storedToDos;
+function storeText() {
+            localStorage.setItem("workToDo", JSON.stringify(storedTodos));
         }
-    }
-
-function storeText () {
-    localStorage.setItem("workToDo", JSON.stringify(storedTodos));
-}
 
 
+
+
+    init();
 
 });
-
-
-currentDay();
-setInterval(currentDay, 1000);
