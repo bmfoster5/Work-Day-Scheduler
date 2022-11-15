dfa1dcfe52ee1616dc57693a8e602db12da06169
bmfoster5 <foster.brandon96@gmail.com>
2022-11-14 23:49:18 -0500

    sixth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var timeBlock = $(".time-block")
 var hour = dayjs().hour();
 
 currentDay();
-    setInterval(currentDay, 1000);
+setInterval(currentDay, 1000);
 // TODO: Add code to display the current date in the header of the page.
 function currentDay() {
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -30,33 +30,32 @@ function renderInput() {
     var h16 = $('#hour-16 .description').val(localStorage.getItem('hour-16'))
     var h17 = $('#hour-17 .description').val(localStorage.getItem('hour-17'))
     window.localStorage.getItem(h9, h10, h11, h12, h13, h14, h15, h16, h17)
-  }
+}
 
-$(document).ready($(function() {
+$(document).ready($(function () {
 
-
-
-    saveButton.on('click', function(event) {
+    saveButton.on('click', function (event) {
         event.preventdefault();
         var workText = userInput.value.trim();
         if (workText === "") {
             return;
-    })
-};
+        }
+    });
     function colorChange() {
-        $(timeBlock).each(function(){
-          var timeSlot = parseInt($(this).attr('id').split('-')[1]);
-          if (timeSlot = hour) {
-            timeBlock.classList.add('present')
-          } else if (timeSlot > hour) {
-            timeBlock.classList.add('future')
-          } else {
-            timeBlock.classList.add('past')
-          }
+        $(timeBlock).each(function () {
+            var timeSlot = parseInt($(this).attr('id').split('-')[1]);
+            if (timeSlot = hour) {
+                $(timeBlock).addClass("present")
+                // remove class
+            } else if (timeSlot > hour) {
+                $(timeBlock).addClass("future")
+            } else {
+                timeBlock.classList.add('past')
+            }
         })
-      }
-      
-      colorChange()
+    }
+
+    colorChange()
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -64,30 +63,27 @@ $(document).ready($(function() {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-  
 
-        storeText();
 
-        // TODO: Add code to apply the past, present, or future class to each time
-        // block by comparing the id to the current hour. HINTS: How can the id
-        // attribute of each time-block be used to conditionally add or remove the
-        // past, present, and future classes? How can Day.js be used to get the
-        // current hour in 24-hour time?
-        function init() {
-            var storedToDos = JSON.parse(localStorage.getItem(storedToDos));
-        
+    storeText();
 
-           
-
-        }
-    };
-
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    function init() {
+        var storedToDos = JSON.parse(localStorage.getItem(storedToDos));
+    }
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-function storeText() {
-            localStorage.setItem("workToDo", JSON.stringify(storedTodos));
-        }
+    function storeText() {
+        localStorage.setItem("workToDo", JSON.stringify(workTodo));
+    }
 
     init();
+}));
+
+
 
